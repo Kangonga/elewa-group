2d76a8bd3f10c05f83eca4073543e29fab5d61de
Kangonga <guchu413@gmail.com>
2023-02-22 10:55:45 +0300

    feat: Add inputs and interpolation to enable data binding for the html content and ngStyle to access component defined styles.

--- libs/pages/elewa/social-impact/src/lib/components/reusable-home-jobs-section/reusable-home-jobs-section.component.ts
+++ libs/pages/elewa/social-impact/src/lib/components/reusable-home-jobs-section/reusable-home-jobs-section.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-reusable-home-jobs-section',
   templateUrl: './reusable-home-jobs-section.component.html',
   styleUrls: ['./reusable-home-jobs-section.component.scss'],
 })
-export class ReusableHomeJobsSectionComponent {}
+export class ReusableHomeJobsSectionComponent {
+  @Input() message:string
+  @Input() callToAction:string
+  @Input() textAlign:string
+  @Input() destinationUrl:string
+}
